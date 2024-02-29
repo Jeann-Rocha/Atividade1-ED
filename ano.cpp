96fef597bdfa344c0692a0b328403d57816f5611
JEANN DA ROCHA SILVA <B43257@fgv.edu.br>
2024-02-29 14:51:00 -0300

    Add ano.cpp

---
+++ ano.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdlib> // for atoi function
+
+using std::cout;
+using std::endl;
+using std::atoi;
+
+int is_leapyear(int year); // function that informs if the year is a leap year or not
+
+int main(int argc, char* argv[]) {
+
+    if (argc != 2) {
+        cout << "Sorry, invalid input! USAGE number." << endl;
+        return 1;
+    }
+
+    int year = atoi(argv[1]);
+
+    is_leapyear(year) == 1 ? cout << "The year informs is a leapyear!" << endl : cout << "The year informs isn't a leapyear!" << endl;
+
+    return 0;
+
+}
+
+int is_leapyear(int year) {
+
+    if (((year % 4 == 0) & (year % 100 != 0)) | (year % 400 == 0)) {
+        return 1; // true
+    } else {
+        return 0; // false
+    }
+
+}
